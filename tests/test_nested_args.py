bb938e599c64db3d0d787d1a507a90f7361b3632
Sami Jaghouar <sami.jaghouar@gmail.com>
2024-05-12 17:19:36 +0200

    refactor tests org
    Signed-off-by: Sami Jaghouar <sami.jaghouar@gmail.com>

--- tests/test_nested_args.py
+++ tests/test_nested_args.py
@@ -1,30 +1,6 @@
 import pytest
-
-from pydantic_config.parse import (
-    DuplicateKeyError,
-    parse_nested_arg,
-    semi_parse_argv,
-    CliArgError,
-)
-
-
-@pytest.mark.parametrize("arg", ["hello", "-hello"])
-def test_no_underscor_arg_failed(arg):
-    argv = ["main.py", arg]
-
-    with pytest.raises(CliArgError):
-        semi_parse_argv(argv)
-
-
-def test_correct_arg_passed():
-    argv = ["main.py", "--hello", "world", "--foo", "bar"]
-    assert semi_parse_argv(argv) == {"hello": "world", "foo": "bar"}
-
-
-def test_duplicate_keys_fail():
-    argv = ["main.py", "--hello", "world", "--hello", "universe"]
-    with pytest.raises(DuplicateKeyError):
-        semi_parse_argv(argv)
+from pydantic_config.error import CliArgError, DuplicateKeyError
+from pydantic_config.parse import parse_nested_arg
 
 
 def test_parse_nested_arg():
---
+++ tests/test_semi_parse.py
@@ -0,0 +1,31 @@
+import pytest
+
+from pydantic_config.parse import (
+    DuplicateKeyError,
+    semi_parse_argv,
+    CliArgError,
+)
+
+
+@pytest.mark.parametrize("arg", ["hello", "-hello"])
+def test_no_underscor_arg_failed(arg):
+    argv = ["main.py", arg]
+
+    with pytest.raises(CliArgError):
+        semi_parse_argv(argv)
+
+
+def test_correct_arg_passed():
+    argv = ["main.py", "--hello", "world", "--foo", "bar"]
+    assert semi_parse_argv(argv) == {"hello": "world", "foo": "bar"}
+
+
+def test_duplicate_keys_fail():
+    argv = ["main.py", "--hello", "world", "--hello", "universe"]
+    with pytest.raises(DuplicateKeyError):
+        semi_parse_argv(argv)
+
+
+def test_python_underscor_replace():
+    argv = ["main.py", "--hello-world", "hye", "--foo_bar", "bar"]
+    assert semi_parse_argv(argv) == {"hello_world": "hye", "foo_bar": "bar"}
