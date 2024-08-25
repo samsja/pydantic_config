821e469769e9f52134edfdc13a4dc5c118d6abaf
Sami Jaghouar <sami.jaghouar@gmail.com>
2024-08-25 22:26:11 +0200

    fix suggestion no bool
    Signed-off-by: Sami Jaghouar <sami.jaghouar@gmail.com>

--- tests/saw_error_message.sh
+++ tests/saw_error_message.sh
@@ -4,4 +4,6 @@ echo "### new line"
 uv run python tests/ui_testing.py --no-hello l --a
 echo "### new line"
 uv run python tests/ui_testing.py hello
+echo "### new line"
+ uv run python tests/ui_testing.py --no-hello world
 
